e3ed2f467aca094764c60c53d5bdd2dbaf434057
Ibrahim Abdelnaby <info.ibrahimabdelnaby@gmail.com>
2024-07-30 15:29:16 +0300

    Complete the UI of the header component.

---
+++ src/components/1-header/Header.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./header.css";
+import "../../../public/style.css";
+
+export default function Header() {
+  const [modal, setModal] = useState(false);
+
+  return (
+    <header className="flex">
+      <button className="mode flex"
+        onClick={() => {
+          setModal(true);
+        }}
+      >
+        <span className="menu icon-menu"/>
+      </button>
+
+      <div />
+
+      <nav>
+        <ul className="flex">
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Articles</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">Speaking</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button className="mode flex">
+        <span className="icon-moon-o"/>
+      </button>
+
+      {modal && (
+        <div className="fixed">
+          <ul className="modal">
+            <li className="flex">
+              <button className="mode flex cancel"
+                onClick={() => {
+                  setModal(false);
+                }}
+              >
+                <span className="icon-cancel" />
+              </button>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Articles</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Speaking</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
+}
